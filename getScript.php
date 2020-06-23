cb07265875f7f6c6455cb94002e9730a8cbd0325
pozm <44528100+pozm@users.noreply.github.com>
2020-06-23 20:11:06 +0100

    Initial commit

---
+++ getScript.php
@@ -0,0 +1,19 @@
+<?php
+
+
+    $sent = null;
+    if (array_key_exists('script',$_GET)) $sent = $_GET['script'];
+
+    if (!$sent) {return;}
+
+    $exists = glob('./'. $sent . '.*');
+
+    $whiteList = ['.txt','.lua','.js'];
+
+    if ($exists && in_array( substr($exists[0], strrpos($exists[0],'.')),$whiteList) ) {
+
+        echo file_get_contents($exists[0]);
+
+    } else echo 'Bad request';
+
+?>
