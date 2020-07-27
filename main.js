aa87bfc7f60dfe765fc2575ab3cf600fbc3b1b2c
pozm <44528100+pozm@users.noreply.github.com>
2020-07-27 19:43:43 +0100

    Started using ejs

--- main.js
+++ main.js
@@ -4,14 +4,19 @@ const fs = require('fs')
 const mysql = require('mysql')
 const rateLimit = require("express-rate-limit");
 const bodyParser = require('body-parser');
-const {mysqlPassWord} = require('./keys')
-
+const {mysqlPassWord, SessionKey, recapSiteKey} = require('./keys');
+const session = require('express-session')
+const crypto = require('crypto');
+const multer = require('multer');
+const got = require('got')
+var MySQLStore = require('express-mysql-session')(session);
 
 const limiter = rateLimit({
-    windowMs: 60e3, 
-    max: 10 
+    windowMs: 180e3, 
+    max: 15 
 });
 
+let upload = multer();
 
 let con = mysql.createConnection({
     host: "localhost",
@@ -31,29 +36,297 @@ app.use('/', express.static(path.join(__dirname, 'html'), {extensions:['html']})
 app.set('trust proxy', true)
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(upload.array());
+
 app.use('/IsGay',limiter)
+app.use('/api/',limiter)
 
-app.get('/getScript', (req, res) => {
+con.query('create database if not exists sessions')
 
-    if (!req.query.script) {
-        res.status(400).send('Bad Request.')
-        return;
+var sessionStore = new MySQLStore({
+
+    host:'localhost',
+    port:3306,
+    user:'root',
+    password:mysqlPassWord,
+    database:'sessions'
+
+});
+
+app.use(session({
+    store: sessionStore,
+    secret: SessionKey,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, 'expires' : 1000 * 60 * 60 * 24 * 30000},
+    name: 'session'
+}))
+
+const scriptsMetaData = JSON.parse(fs.readFileSync('p-Scripts/meta.json'))
+
+
+async function getDataFromId (id) {
+    
+    return new Promise(res => {
+        
+        con.query('create database if not exists whitelist')
+        con.query('use whitelist')
+        con.query(`create table if not exists \`whitelist\`.\`account\` (
+            
+            \`id\` int primary key not null auto_increment,
+            \`username\` varchar(15) not null,
+            \`password\` text not null,
+            \`email\` text not null
+            
+            )
+            `)
+            
+            con.query('select * from `whitelist`.`account` where id = ?',id, (err, res2) => {
+                
+                if (err) throw err
+                if (0 in res2) return res( res2[0])
+                res(null)
+            })
+            
+            
+        })
+        
     }
-    let f = path.join(__dirname, `p-Scripts/${req.query.script}.js`);
-    if (!fs.existsSync(f)) return res.status(400).send('Bad Request.')
-    res.sendFile(f)
+    
+    
+    async function AsyncQuery() {
+        
+        return new Promise(resolve => {
+            
+            func = (err,result)=>{
+                
+                if (err) throw err
+                resolve(result)
+                
+            }
+            con.query(arguments[0],arguments[1] ?? func, func)
+            
+        })
+        
+    }
+    
+    
+    
+    // front end
+    
+    
+    app.get('/', async (req,res) => {
+        
+        let data = await getDataFromId(req.session.logedInto ?? '')
+        
+        res.render('index',{data : data ? data : false})
+    })
+    
+    app.get('/login', async (req,res) => {
+        
+        let data = await getDataFromId(req.session.logedInto ?? '')
+        if (data) return res.redirect('/')
+        
+        res.render('login',{signedInto : data ? data.username : false})
+    })
+    app.get('/signup', async (req,res) => {
+        
+        let data = await getDataFromId(req.session.logedInto ?? '')
+
+        if (data) return res.redirect('/')
+        
+        res.render('signup',{signedInto : data ? data.username : false})
+    })
+    
+    app.get('/admin', async (req,res) => {
+        
+        let data = await getDataFromId(req.session.logedInto ?? '')
+        if (!data) return res.status(401).sendFile(path.join(__dirname, 'html/errors/401.html'))
+        if (data.powerId < 5) return res.status(401).sendFile(path.join(__dirname, 'html/errors/401.html'))
+        let tables = await AsyncQuery('select * from `whitelist`.`account`')
+        
+        res.render('admin',{data : data ? data : false, reg : tables})
+    })
+    
+    
+    app.get('/getScript', async (req, res) => {
+
+        if (!req.query.script) {
+            res.status(400).send('Bad Request.')
+            return;
+        }
+        let scr = req.query.script
+        if (!scriptsMetaData[scr]) {
+            res.status(400).send('Bad Request.')
+            console.log('1')
+            return;
+        }
+        let data = await getDataFromId(req.session.logedInto ?? '')
+        console.log(data)
+        if (scriptsMetaData[scr].power > 0 && (! data) ) return res.sendStatus(401)
+        if (data.powerId < scriptsMetaData[scr].power) return res.sendStatus(401)
+        res.sendFile(path.join(__dirname,'p-Scripts/'+scriptsMetaData[scr].url))
+    
+    })
+    
+    
+    //api / backend
+    
+async function getIdFromUser (user) {
+
+    return new Promise(res => {
+
+        con.query('select id from `whitelist`.`account` where username = ?',user, (err, res2) => {
+
+            if (err) throw err
+            if (0 in res2) return res( res2[0].id)
+            res(false)
+        })
+
+
+    })
+
+}
+
+app.post('/admin/UpdatePower', async (req,res) => {
+
+    let data = await getDataFromId(req.session.logedInto ?? '')
+    if (!data) return res.status(401).sendFile(path.join(__dirname, 'html/errors/401.html'))
+    if (data.powerId < 5 ) return res.status(401).sendFile(path.join(__dirname, 'html/errors/401.html'))
+    let {id,power} = req.body
+    let data2 = await getDataFromId(id ?? '')
+    if (!data2) return res.sendStatus(400)
+    if (power > data.powerId) return res.status(401).sendFile(path.join(__dirname, 'html/errors/401.html'))
+    console.log(power,id)
+    await AsyncQuery('update `whitelist`.`account` set powerId = ? where id = ?',[power,id])
+    return res.json({message:'Successfully updated'})
+})
+
+app.delete('/admin/AccountDelete', async (req,res) => {
+
+
+    let data = await getDataFromId(req.session.logedInto ?? '')
+    if (!data) return res.status(401).sendFile(path.join(__dirname, 'html/errors/401.html'))
+    if (data.powerId < 5 ) return res.status(401).sendFile(path.join(__dirname, 'html/errors/401.html'))
+    let {id} = req.body
+    let data2 = await getDataFromId(id ?? '')
+    if (!data2) return res.sendStatus(400)
+    if (data2.powerId >= data.powerId) return res.status(401).sendFile(path.join(__dirname, 'html/errors/401.html'))
+    console.log(id)
+    await AsyncQuery('delete from `whitelist`.`account` where id = ?',id)
+    return res.json({message:'Successfully deleted'})
+})
+
+app.get('/api/logout', (req,res) => {
+
+    req.session.destroy()
+    res.send('<script>window.location.replace("/")</script>')
 
 })
 
-app.post('/api/CreateAccount', (req,res) => {
+app.post('/api/LogintoAccount', (req,res) => {
+    
+    req.session.cool = true
+    let {username,password} = req.body
+    if (!password) return res.status(400).send(JSON.stringify({error:19,message:'Missing credentials'}))
+    if (! username) return res.status(400).send(JSON.stringify({error:19,message:'Missing credentials'}))
+    
+    con.query('create database if not exists whitelist')
+    con.query('use whitelist')
+    con.query(`CREATE TABLE if not exists \`account\` (
+        \`id\` int NOT NULL AUTO_INCREMENT,
+        \`username\` varchar(15) NOT NULL,
+        \`password\` text NOT NULL,
+        \`email\` text NOT NULL,
+        \`registeredAt\` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+        \`powerId\` int NOT NULL DEFAULT '0',
+        \`subscriptions\` json,
+        PRIMARY KEY (\`id\`)
+        )
+      
+    `)
+    hash = crypto.createHash('sha512')
+        .update(password)
+    let digested = hash.digest('hex')
+    console.log(digested)
+    con.query('select * from `whitelist`.`account` where password = ? and (username = ? or email = ?)', [digested, username ?? '', username ?? ''], (err,resi) => {
+        if (err) throw err
+        console.log(resi)
+        if (0 in resi) {
+
+            console.log(resi,digested)
+
+            req.session.logedInto = resi[0].id
+            res.send(JSON.stringify({error : false, message : 'Logged in'}))
+        }
+        else {
+            res.status(401).send(JSON.stringify({error:6, message:'Invalid credentials'}))
+        }
+    })
+})
+
+
+
+app.post('/api/CreateAccount', async (req,res) => {
 
     console.log(req.body)
-    let {username,password,key} = req.body
-    if (! username||!password||!key) return res.sendStatus(400)
+
+    if (!req.body['g-recaptcha-response']) return res.json({error:21,message:'Please complete the recaptcha'})
+
+    var verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" + recapSiteKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+
+    let resb = JSON.parse((await got(verificationUrl)).body)
+
+    console.log(resb)
+
+    if (!resb.success) return res.json({error:21,message:'You have failed the recaptcha, try again?'})
+
+    let {username,password,email} = req.body
+    if (! username||!password||!email) return res.status(400).send(JSON.stringify({error:19,message:'Missing credentials'}))
+
+    if (!email.match(/.{0,64}[@](\w{0,255}[.])\w{0,10}/)) return res.status(400).send(JSON.stringify({'error':3,message:'bad email'}))
+    if (username.match(/(.{30}|[^A-Za-z\d])/)) return res.status(400).send(JSON.stringify({'error':4,message:'bad username'}))
+    if (!password.match(/(.{32}|^[\x00-\x7F]+$)/)) return res.status(400).send(JSON.stringify({'error':7,message:'password is invalid'}))
 
     con.query('create database if not exists whitelist')
     con.query('use whitelist')
-    con.query(`CREATE TABLE if not exists \`whitelist\`.\`keycode\` (
+    con.query(`CREATE TABLE if not exists \`account\` (
+        \`id\` int NOT NULL AUTO_INCREMENT,
+        \`username\` varchar(15) NOT NULL,
+        \`password\` text NOT NULL,
+        \`email\` text NOT NULL,
+        \`registeredAt\` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+        \`powerId\` int NOT NULL DEFAULT '0',
+        \`subscriptions\` json,
+        PRIMARY KEY (\`id\`)
+        )
+      
+    `)
+    con.query('select * from `whitelist`.`account` where username = ? or email = ?', [username,email], (err , resu) => {
+
+        if (err) throw err
+        if (0 in resu) {
+
+            return res.send(JSON.stringify({'error' : 10, message: `${0 in resu.filter(v => v.email == email) ? 'Email' : 'Username'} is already in use.`}))
+
+        }
+        else {
+
+            hash = crypto.createHash('sha512')
+                .update(password)
+
+            con.query('insert into account (username,password,email) values (?, ?, ?)', [username,hash.digest('hex'),email], async(err,resu,fie) => {
+                if (err) throw err
+                req.session.logedInto = await getIdFromUser(username)
+                res.send(JSON.stringify({'message':'sucessfully logged in'}))
+            })
+
+        }
+
+    })
+
+/*     con.query(`CREATE TABLE if not exists \`whitelist\`.\`keycode\` (
         \`id\` INT primary key NOT NULL AUTO_INCREMENT,
         \`key\` VARCHAR(255) NOT NULL,
         \`registered\` bool NOT NULL DEFAULT false,
@@ -62,7 +335,7 @@ app.post('/api/CreateAccount', (req,res) => {
     `)
     con.query('select * from keycode where "key" = ?',key, (err,resul,fe) => {
         if (! 0 in resul) return res.sendStatus(401)
-    })
+    }) */
 
 })
 
@@ -82,11 +355,11 @@ function toByteCode(s) {
 app.get('/IsGay', (req,res) => {
 
 
-    if (req.headers["user-agent"] == 'python-requests/2.23.0' || ! req.headers.cookie) return res.sendStatus(403)
+    if (req.headers["user-agent"] == 'python-requests/2.23.0' /* || ! req.headers.cookie  */) return res.sendStatus(403)
 
     let user = req.query.user
     let reason = req.query.reason ?? 'He is gay.'
-    if (!user) return res.sendStatus(403)
+    if (!user) return res.render('makeIsGay')
     try {user = decodeURI(user)} catch (e) {return res.send('fuck off u entered a bad url') }
 
     let test = user.match(/\S/gi) ?? ['']
@@ -105,7 +378,7 @@ app.get('/IsGay', (req,res) => {
 
 })
 
-app.get('/IsGayLog', (req,res) => {
+app.get('/IsGay/Log', (req,res) => {
 
     con.query('create database if not exists IsGayGlobalLogs')
     con.query('use IsGayGlobalLogs')
